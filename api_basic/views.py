1882e9595fdd288ff4382d884b092c16141532b0
Blademaster680 <dylanprimic@gmail.com>
2020-03-18 15:51:12 +0200

    6 - REST Class Based API Views
    https://www.youtube.com/watch?v=B38aDwUpcFc&t=4097s

--- api_basic/views.py
+++ api_basic/views.py
@@ -7,8 +7,58 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 # Create your views here.
+
+class ArticleAPIView(APIView):
+
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+
+        # If form is valid
+        if serializer.is_valid():
+            # We save the data in the form
+            serializer.save()
+            # And we return the data as a JSON Response
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # If form is invalid
+        return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetailsView(APIView):
+
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+
+        except Article.DoesNotExist:
+            return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET', 'POST'])
 def article_list(request):
@@ -21,13 +71,9 @@ def article_list(request):
     elif request.method == 'POST':
         serializer = ArticleSerializer(data=request.data)
 
-        # If form is valid
         if serializer.is_valid():
-            # We save the data in the form
             serializer.save()
-            # And we return the data as a JSON Response
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # If form is invalid
         return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
 
 
