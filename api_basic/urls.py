2a05c4163920b17ecc9fa19a97cfc4c6e5ab27ea
Blademaster680 <dylanprimic@gmail.com>
2020-03-18 17:04:50 +0200

    7 - REST Generic Views & Mixins
    https://www.youtube.com/watch?v=B38aDwUpcFc&t=5094s

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import article_list, article_detail, ArticleAPIView, ArticleDetailsView
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetailsView, GenericAPIView
 
 urlpatterns = [
     #path('api/', article_list),
     path('api/', ArticleAPIView.as_view()),
     path('detail/<int:id>/', ArticleDetailsView.as_view()),
+    path('generic/api/<int:id>/', GenericAPIView.as_view()),
 ]
